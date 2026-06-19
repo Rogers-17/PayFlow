02447420d7fdb49fb64f253e078e1c855d18a9a0
Rogers <mohammedrogers579@gmail.com>
2026-06-19 14:13:50 -0800

    update(hero): edit hero content, add playstore & appstore btns

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/ui/Button";
 import GIF from '@/assets/images/Collect-Payments-for-Multiple-Channels-1.1.gif'
 import Image from "next/image";
+import { Download, Smartphone } from "lucide-react";
 
 export default function Hero (){
     return(
@@ -9,20 +10,21 @@ export default function Hero (){
                <div className="grid grid-cols-1 md:grid-cols-2 gap-20 md:pb8 lg:pb-10">
                     <div>
                         <h1 className="text-4xl md:text-5xl lg:text-6xl tracking-normal lg:leading-16 font-bold max-w-2xl">
-                            Move Your Money Freely with PayFlow
+                            Global Shopping, Local Money with SnapCard
                         </h1>
                         <p className="mt-5 text-xl text-white/60">
-                            The simplest way to send, receive, 
-                            and manage your money across borders. 
-                            Fast, secure, and zero-fee domestic transfers.
+                            Create instant virtual Visa cards for online shopping, subscriptions, and more.
+                            Fund them directly and instantly right from your mobile money wallet.
                         </p>
                         <div className="mt-5 flex gap-5 flex-col md:flex-row">   
                             <Button
-                            className="px-5 py-3 bg-purple-500 rounded-lg"
-                            >Get Started for Free</Button>
+                            className="px-5 py-3 bg-purple-500 rounded-lg
+                            flex items-center gap-2 text-lg"
+                            >App Store <Download /></Button>
                             <Button
-                            className="px-5 py-3 bg-white text-primary rounded-lg"
-                            >See How It Works</Button>
+                            className="px-5 py-3 bg-white text-primary rounded-lg 
+                            flex items-center gap-2 text-lg"
+                            >Play Store <Smartphone /></Button>
                         </div>
                     </div>
                     <div>
