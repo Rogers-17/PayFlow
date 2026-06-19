3f279da4542078a88ad00350cfb917de9d598488
Rogers <mohammedrogers579@gmail.com>
2026-06-19 14:15:17 -0800

    update(logo): edit from payflow to snapcard

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,12 +1,12 @@
 export default function Logo (){
     return (
         <div className="h-10 flex gap-1 items-center">
-            <div className="h-6 w-6 flex justify-center items-center 
-            rounded-full bg-white text-primary text-sm">
-                P
+            <div className="h-8 w-8 flex justify-center items-center 
+            rounded-full bg-white text-primary">
+                S
             </div>
             <p className="text-xl font-medium leading-none">
-                PayFlow
+                SnapCard
             </p>
         </div>
     )
